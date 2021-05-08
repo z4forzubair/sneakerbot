5eda775d357172902be29ab4a8a0d3506d51b16e
M Zubair <zuabirpunjab786@gmail.com>
2021-05-08 05:39:56 +0500

    Added models and created new app churchaio -- to further look into images yet

--- sneakerbot_backend/settings.py
+++ sneakerbot_backend/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'churchaio.apps.ChurchaioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
