b55c959aba5f7944bd46a5e954e60a0cc1584d23
Shoaib <shoaib.ahmedd06@gmail.com>
2021-05-09 22:53:41 +0500

    Refactored login page and intgrated ui template

---
+++ churchaio/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path
+from churchaio import views
+
+
+urlpatterns = [
+
+    # The home page
+    path('', views.index, name='home'),
+
+    # Matches any html file
+    re_path(r'^.*\.*', views.pages, name='pages'),
+]
