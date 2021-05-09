26eedbd8dfeb6e95f248f3fd47de97af19882ded
Shoaib <shoaib.ahmedd06@gmail.com>
2021-05-10 02:08:39 +0500

    Removed unused ui components

--- sneakerbot_backend/settings.py
+++ sneakerbot_backend/settings.py
@@ -130,3 +130,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'home'
