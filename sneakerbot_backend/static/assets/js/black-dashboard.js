234fae45ae91b02868c3ca020aa7ee438aa6eaee
Shoaib <shoaib.ahmedd06@gmail.com>
2021-05-13 00:32:03 +0500

    Made CRUD buttons responsive and removed copyright comments

--- sneakerbot_backend/static/assets/js/black-dashboard.js
+++ sneakerbot_backend/static/assets/js/black-dashboard.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Black Dashboard - v1.0.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 var transparent = true;
 var transparentDemo = true;
 var fixedTop = false;
