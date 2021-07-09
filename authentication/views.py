8cc182d415448bcf841c3b37fd1255488d10d79f
Zubair <42717176+z4forzubair@users.noreply.github.com>
2021-07-10 02:54:31 +0500

    Revert "fixed max logged in users limit at 10"

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.contrib.sessions.models import Session
 from django.forms.utils import ErrorList
 from django.http import HttpResponse
 from .forms import LoginForm, SignUpForm
@@ -13,8 +12,8 @@ from .forms import LoginForm, SignUpForm
 def login_view(request):
     if request.user.is_authenticated:
         return redirect('home')
-
     form = LoginForm(request.POST or None)
+
     msg = None
 
     if request.method == "POST":
@@ -24,16 +23,8 @@ def login_view(request):
             password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=password)
             if user is not None:
-                try:
-                    sessions = Session.objects.all()
-                except Exception as ex:
-                    msg = 'Some issue found with sessions'
-                else:
-                    if sessions.count() < 10:
-                        login(request, user)
-                        return redirect("/")
-                    else:
-                        msg = 'Already reached max users limit!'
+                login(request, user)
+                return redirect("/")
             else:
                 msg = 'Invalid credentials'
         else:
@@ -59,16 +50,8 @@ def register_user(request):
             msg = 'User created - please <a href="/login">login</a>.'
             success = True
             if user is not None:
-                try:
-                    sessions = Session.objects.all()
-                except Exception as ex:
-                    msg = 'Some issue found with sessions'
-                else:
-                    if sessions.count() < 10:
-                        login(request=request, user=user)
-                        return redirect('userProfile')
-                    else:
-                        msg = 'User created, but already reached max logged in users limit!'
+                login(request=request, user=user)
+                return redirect('userProfile')
 
         else:
             msg = 'Form is not valid'
