436ef17c182e8b4edf304cf5321c5d420327886a
M Zubair <zubairpunjab786@gmail.com>
2021-09-05 20:47:54 +0500

    refactored signup related code in view, url, form, template removed buttons from sidebar moved the unused files to the archived folder removed unused buttons on proxies, tasks minor changes

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -18,59 +16,3 @@ class LoginForm(forms.Form):
                 "class": "form-control"
             }
         ))
-
-
-class SignUpForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
-        self.instance = kwargs.pop('instance', None)
-        super(SignUpForm, self).__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
-
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "First Name"
-            }
-        ))
-    last_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "Last Name"
-            }
-        ))
-    username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "Username"
-            }
-        ))
-    email = forms.EmailField(
-        widget=forms.EmailInput(
-            attrs={
-                "placeholder": "Email"
-            }
-        ))
-    password1 = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Password"
-            }
-        ))
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "Confirm Password"
-            }
-        ))
-
-    def clean(self):
-        cd = self.cleaned_data
-        if User.objects.filter(email=cd.get('email')).exists():
-            self.add_error('email', "Email already exists !")
-        return cd
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
