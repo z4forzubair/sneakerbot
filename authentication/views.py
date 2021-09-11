cd6e6f4411d9347c5ae6add62f0e0f6d6f433fee
M Zubair <zubairpunjab786@gmail.com>
2021-09-11 19:27:40 +0500

    logout and redirect to login on tasks page if subscription ended

--- authentication/views.py
+++ authentication/views.py
@@ -3,10 +3,26 @@ import datetime
 
 from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.views import LogoutView
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 from churchaio.alerts import LOGIN_ALERTS, GENERAL_ALERTS
 from .forms import LoginForm
+
+
+class MyLogoutView(LogoutView):
+    # modifying the method inside LogoutView
+    def dispatch(self, request, *args, **kwargs):
+        expired = request.session.get("expired")
+        auth_logout(request)
+        request.session["expired"] = expired
+        next_page = self.get_next_page()
+        if next_page:
+            # Redirect to this page until the session has been cleared.
+            return HttpResponseRedirect(next_page)
+        return super().dispatch(request, *args, **kwargs)
 
 
 def login_view(request):
@@ -16,7 +32,7 @@ def login_view(request):
 
     success = None
     msg = None
-    expired = None
+    expired = request.session.get('expired')
     if request.method == "POST":
 
         if request.session.get('_new_sub') is None:
@@ -34,8 +50,6 @@ def login_view(request):
                         redirect_to = "userProfile" if user.last_login is None else "home"
                         login(request, user)
                         return redirect(redirect_to)
-                    elif expired == True:
-                        msg = LOGIN_ALERTS.get("expired")
                 else:
                     success = False
                     msg = LOGIN_ALERTS.get("invalid")
@@ -43,34 +57,35 @@ def login_view(request):
                 success = False
                 msg = LOGIN_ALERTS.get("form_error")
 
-    new_user = request.session.get('_new_user')
-    if new_user == True:
-        request.session['_new_user'] = None
-        success = True
-        msg = LOGIN_ALERTS.get("stripe_success")
-    elif new_user == False:
-        request.session['_new_user'] = None
-        success = False
-        msg = LOGIN_ALERTS.get("stripe_fail")
-
-    new_sub = request.session.get('_new_sub')
-    if new_sub == True:
-        request.session['_new_sub'] = None
-        success = True
-        msg = LOGIN_ALERTS.get("stripe_subscribe")
-    elif new_sub == False:
-        request.session['_new_sub'] = None
-        success = False
-        msg = LOGIN_ALERTS.get("stripe_fail")
-
-    context = {
-        "form": form,
-        "banner_msg": msg,
-        "success": success
-    }
-
+    context = {}
     if expired:
+        request.session["expired"] = None
+        msg = LOGIN_ALERTS.get("expired")
         context["STRIPE_PUBLIC_KEY"] = settings.STRIPE_PUBLIC_KEY
         context["product_id"] = 2
+    else:
+        new_user = request.session.get('_new_user')
+        if new_user == True:
+            request.session['_new_user'] = None
+            success = True
+            msg = LOGIN_ALERTS.get("stripe_success")
+        elif new_user == False:
+            request.session['_new_user'] = None
+            success = False
+            msg = LOGIN_ALERTS.get("stripe_fail")
+
+        new_sub = request.session.get('_new_sub')
+        if new_sub == True:
+            request.session['_new_sub'] = None
+            success = True
+            msg = LOGIN_ALERTS.get("stripe_subscribe")
+        elif new_sub == False:
+            request.session['_new_sub'] = None
+            success = False
+            msg = LOGIN_ALERTS.get("stripe_fail")
+
+    context["form"] = form
+    context["banner_msg"] = msg
+    context["success"] = success
 
     return render(request, "accounts/login.html", context=context)
